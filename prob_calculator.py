d7eab8b20a265e0d2fa418501a13193a8e8e8922
EzekielUmanmah <umanmah234@gmail.com>
2021-03-23 00:14:05 +0000

    Completed the project.

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,37 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        self.copy = []
 
+        for color, n in kwargs.items():
+            #self.contents+=[color] * n
+            self.copy+=[color] * n
+            self.contents = self.copy.copy()
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    def draw(self, n):
+        self.contents = self.copy.copy()
+        if n > len(self.contents): return self.contents
+        drawn = random.sample(self.contents,k=n)
+        for color in drawn:
+            self.contents.remove(color)
+        
+        return drawn
+
+def experiment(**kwargs):
+
+    (hat, expected_balls, num_balls_drawn, num_experiments) = kwargs.values()
+
+    M = 0
+    i = num_experiments
+    while i > 0:
+        drawnBalls = hat.draw(num_balls_drawn)
+        drawnBallsDict = dict()
+        
+        for color, n in expected_balls.items():
+            if color in drawnBalls and drawnBalls.count(color) >= n:
+                drawnBallsDict[color] =  n
+        if drawnBallsDict == expected_balls: M+=1
+        i-=1
+    return M/num_experiments
